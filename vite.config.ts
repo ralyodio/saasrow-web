7c183225fc7c8798751b8632e43c2786a69dd26e
Anthony Ettinger <anthony@chovy.com>
2025-10-24 06:07:35 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,17 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 5173,
+    strictPort: false,
+    open: false,
+  },
+  preview: {
+    port: 4173,
+    strictPort: false,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
