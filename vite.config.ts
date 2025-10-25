0fecbdba8244c7d89af24dcfaab16584522ffd62
Anthony Ettinger <anthony@chovy.com>
2025-10-24 20:10:18 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import fs from 'fs'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const envPath = path.resolve(process.cwd(), '.env')
+  const env = fs.existsSync(envPath) ? loadEnv(mode, process.cwd(), '') : {}
 
   return {
   plugins: [react()],
